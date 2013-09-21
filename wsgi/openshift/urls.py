361afaabf576de90e7d19a72eec0bb309c9390d2
Kevin Weirauch <kevin@weirau.ch>
2013-09-21 18:59:26 -0400

    Tweaked website.  Added about and marketplace

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^about/', 'openshift.views.about', name='about'),
+	url(r'^marketplace/', 'openshift.views.marketplace', name='marketplace'),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
