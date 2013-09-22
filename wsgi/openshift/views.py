8cc0e8a730da3e611bd0b4ead585e7a1a24f78af
cvolny <cvolny@gmail.com>
2013-09-22 14:03:07 -0400

    user view and sample data

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -8,7 +8,10 @@ def about(request):
     return render_to_response('home/about.html')
 
 def user(request):
-    return render_to_response('home/user.html')
+    from openshift.shims.user import files
+    return render_to_response('home/user.html', {
+        'files': files
+    })
 
 def marketplace(request):
     return render_to_response('marketplace.html')
